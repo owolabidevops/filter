ebdfcc334e5f266db8bdb35a1184dd3757552752
Owolabi <48913682+owolabidevops@users.noreply.github.com>
2020-04-18 13:11:38 +0100

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,7 @@
+//it gives you a new array with all the values from a given array that satisfy given condition or test
+
+
+const numbers=[1, 2, 3, 4, 10];
+ const num =numbers.filter(x => x%2===0);
+ console.log(num)//returns [2,4,10]
+ 
